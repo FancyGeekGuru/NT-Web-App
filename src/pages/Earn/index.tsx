36825f8e5c4249375d8b1635571af0036784cdf4
$uper Dev <94814005+song0411@users.noreply.github.com>
2022-05-17 23:02:52 +0900

    tomio touched earn page..

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -1,17 +1,68 @@
 import * as React from 'react';
-import { Container, ButtonGroup, Button} from 'react-bootstrap';
+import { Container, ButtonGroup, Button, Row, Col} from 'react-bootstrap';
 import './index.scss';
 const Earn = () => {
     return (
         <>
             <Container>
-                <p className="page-title"> DASHBOARD </p>
-                <div className="total-balance-container">
-                    <p className="total-balance-text">TOTAL BALANCE<br />532.875USD</p>
+                <p className="page-title"> EARN </p>
+                <div className="NT-Card mt-5 total-balance-container">
+                    <p className="mb-0" style={{ fontSize: '18px', letterSpacing: '-0.06rem' }}>TOTAL BALANCE</p>
+                    <p style={{ fontSize: '30px', fontWeight: '500' }}>532.875 <span style={{ fontSize: '16px' }}>USD</span></p>
                     <ButtonGroup className="total-balance-buttongroup">
                         <Button className="deposit-button">Deposit</Button>
                         <Button className="withdraw-button"> Withdraw</Button>
                     </ButtonGroup>
+                </div>
+                <div>
+                    <Row>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginRight: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginLeft: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginRight: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginLeft: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginRight: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginLeft: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginRight: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                        <Col sm = "6">
+                            <div className="deposit-card" style={{marginLeft: '65px'}}>
+                                
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
             </Container>
         </>
