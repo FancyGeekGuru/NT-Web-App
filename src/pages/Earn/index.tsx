9dc99eab3c5e51794d1670b18dc52cfe575f1c0b
fancydev0225 <fancydev0225@gmail.com>
2022-05-21 19:11:29 -0700

    fancy added Earn page

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { Container} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import externalLink from 'assets/img/External_Link.svg';
+import { coins } from '../data';
+
 const Earn = () => {
     return (
         <>
-            <Container>
+            <Container className='mb-5'>
                 <div className='d-flex align-items-baseline'>
                     <p className="page-title"> EARN </p>
                     <div style={{ cursor: 'pointer' }}>
@@ -14,12 +16,49 @@ const Earn = () => {
                 </div>
                 <div className="NT-Card mt-4">
                     <p className="mb-0" style={{ fontSize: '17px', letterSpacing: '-0.01rem', fontFamily: 'SF Pro Text Heavy' }}>TOTAL BALANCE</p>
-                    <p style={{ fontSize: '28px', fontFamily: 'SF Pro Text Heavy', paddingBottom:'50px'}}>532.875 <span style={{ fontSize: '17px', fontFamily: 'SF Pro Text Heavy' }}>USD</span></p>
-                    <div style={{float: 'right'}}>
-                        <button className='deposit-but'>Deposit</button>
-                        <button className='withdraw-but'>Withdraw</button>
+                    <p style={{ fontSize: '28px', fontFamily: 'SF Pro Text Heavy', paddingBottom: '50px' }}>532.875 <span style={{ fontSize: '17px', fontFamily: 'SF Pro Text Heavy' }}>USD</span></p>
+                    <div className='d-flex' style={{ justifyContent: 'right' }}>
+                        <button className='my-page-deposit-but'>Deposit</button>
+                        <button className='my-page-withdraw-but'>Withdraw</button>
                     </div>
                 </div>
+
+                <Row className='mt-5'>
+                    {
+                        coins.map((coin, index) => {
+                            return (
+                                <Col sm='6' key={index} style={{ marginTop: '30px' }}>
+                                    <div className='NT-Card'>
+                                        <div className='d-flex justify-content-between align-items-center'>
+                                            <div className='d-flex align-items-center'>
+                                                <img src={coin.url} />
+                                                <div className='d-flex flex-column ml-4'>
+                                                    <span style={{ fontFamily: 'SF Pro Text Heavy', fontSize: '16px' }}>{coin.identifier}</span>
+                                                    <span style={{ fontFamily: 'SF Pro Text', fontSize: '13px', color: '#CEC0C0' }}>{coin.name}</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='d-flex flex-column ml-4'>
+                                                <span style={{ fontFamily: 'SF Pro Text Bold', fontSize: '13px', color: '#CEC0C0' }}>Savings Balance</span>
+                                                <span style={{ fontFamily: 'SF Pro Text', fontSize: '13px', color: '#CEC0C0' }}>{'$' + coin.tValue}</span>
+                                            </div>
+
+                                            <div className='d-flex flex-column ml-4'>
+                                                <span style={{ fontFamily: 'SF Pro Text Bold', fontSize: '13px', color: '#CEC0C0' }}>APY</span>
+                                                <span style={{ fontFamily: 'SF Pro Text', fontSize: '13px', color: '#CEC0C0' }}>{coin.APY + '%'}</span>
+                                            </div>
+                                        </div>
+
+                                        <div className='d-flex justify-content-center mt-5 mb-2'>
+                                            <button className='my-page-deposit-but'>Deposit</button>
+                                            <button className='my-page-withdraw-but'>Withdraw</button>
+                                        </div>
+                                    </div>
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
             </Container>
         </>
     );
